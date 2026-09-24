d788dc2bc0aacbbba4b8eb55df8a1600236c3f31
Rashid Khan <rk8539100@gmail.com>
2026-09-24 10:00:49 +0530

    day 44: add failing python test for CI verification

---
+++ tests/math_operations.py
@@ -0,0 +1,18 @@
+import sys
+
+def add(a, b):
+    return a + b
+
+print("=== Running Unit Tests")
+
+# Intentionally making it FAIL first (2+2 != 5)
+expected = 5
+result = add(2, 2)
+
+if result == expected:
+    print("TEST PASSED: MAth works!")
+    sys.exit(0)
+
+else:
+    print(f"TEST FAILED: Expected {expected} but got {result}")
+    sys.exit(1)
